055aa2c027b106e92124a151d66959afe13a160f
AMA2581 <amir.abravesh.ama@gmail.com>
2023-06-29 11:13:26 +0330

    Using the Dismissible Widget for Dismissing List Items

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -43,6 +43,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +64,11 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('the chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+              child: ExpensesList(
+            expenses: _registeredExpenses,
+            onRemoveExpense: _removeExpense,
+          )),
         ],
       ),
     );
